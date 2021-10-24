8b5498d301fd8456226c6bfb95968efb7eb8f950
Piotr Skrzynski <pio.skrzynski@gmail.com>
2021-10-24 20:30:53 +0200

    initial commit on github

--- src/utils/books.tsx
+++ src/utils/books.tsx
@@ -1,6 +1,5 @@
-const searchData = [
+const books = [
   {
-    // name: 'Fixed TOC - table of contents for WordPress plugin',
     name: 'Book Fixed TOC - table of contents for WordPress plugin',
     regularPrice: 20,
     salePrice: 3.99
@@ -11,15 +10,40 @@ const searchData = [
     salePrice: 3.99
   },
   {
-    name: 'Book Lorem ipsum',
+    name: `Harry Potter: Philosopher's Stone`,
     regularPrice: 99,
     salePrice: 21
   },
   {
-    name: 'Book Agata Skrzyński',
+    name: `Harry Potter: Chamber of Secrets`,
+    regularPrice: 99,
+    salePrice: 21
+  },
+  {
+    name: `Harry Potter: Prisoner of Azkaban `,
+    regularPrice: 99,
+    salePrice: 21
+  },
+  {
+    name: `Harry Potter: Goblet of Fire  `,
+    regularPrice: 99,
+    salePrice: 21
+  },
+  {
+    name: `Harry Potter: Order of the Phoenix`,
+    regularPrice: 99,
+    salePrice: 21
+  },
+  {
+    name: `Harry Potter: Half-Blood Prince`,
+    regularPrice: 99,
+    salePrice: 21
+  },
+  {
+    name: `Harry Potter: Deathly Hallows`,
     regularPrice: 99,
     salePrice: 21
   }
 ];
 
-export default searchData;
+export default books;
